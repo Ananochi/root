f4ee41c33cc4efec9903d26d33d928c3eaae90f2
RLOC <rloc@itu.dk>
2016-01-03 13:33:30 +0100

    added exam init

--- PRCPP/Mandatory Assignment 6/Assignment6/src/Main.java
+++ PRCPP/Mandatory Assignment 6/Assignment6/src/Main.java
@@ -3,9 +3,5 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        String test = basicProcessing.n2s(10);
-        int test2 = basicProcessing.randomNum(100);
-
-        System.out.println("le test is le ongoing!!! .....");
     }
 }
